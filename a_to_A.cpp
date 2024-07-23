11205206968f122ef7138cdc07e020e709e019d5
K-Raze <dhamijakartik2005@gmail.com>
2024-07-23 23:55:58 +0530

    convert a small case character to upper

---
+++ a_to_A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void convert(char &alpha)
+{
+    cout << char(alpha - 'a' + 'A') << endl;
+}
+
+int main()
+{
+    char alpha;
+    cout << "Enter a small case alphabet :";
+    cin >> alpha;
+    convert(alpha);
+
+    return 0;
+}
